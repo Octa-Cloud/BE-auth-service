064030464ebc2e1d7c6d4c7761be1aa528fc90d6
KoungQ <guu119@naver.com>
2025-10-09 19:33:49 +0900

    feat: verify authenticate api

---
+++ src/main/java/com/project/auth/domain/ui/controller/VerifyAuthenticationController.java
@@ -0,0 +1,18 @@
+package com.project.auth.domain.ui.controller;
+
+import com.project.auth.domain.ui.controller.spec.VerifyAuthenticationApiSpec;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VerifyAuthenticationController implements VerifyAuthenticationApiSpec {
+
+    @Override
+    public ResponseEntity<Void> verify(
+            Long userNo
+    ) {
+        return ResponseEntity.ok()
+                .header("X-User-Id", userNo.toString()) // todo: userNo Long -> String
+                .build();
+    }
+}
---
+++ src/main/java/com/project/auth/domain/ui/controller/spec/VerifyAuthenticationApiSpec.java
@@ -0,0 +1,19 @@
+package com.project.auth.domain.ui.controller.spec;
+
+import com.project.auth.global.annotation.CurrentUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Tag(name = "Auth")
+public interface VerifyAuthenticationApiSpec {
+
+    @Operation(
+            summary = "인증이 필요한 요청의 인증을 담당하는 API"
+    )
+    @PostMapping("/api/auth/internal")
+    ResponseEntity<Void> verify(
+        @CurrentUser Long userNo
+    );
+}
